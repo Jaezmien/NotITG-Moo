2a7e94e76487c58b644383d8f08824ac3f6c341e
Jaezmien Naejara <jaezmien.naejara@gmail.com>
2021-09-17 19:14:29 +0800

    Remove File attr

--- bg/bg.lua
+++ bg/bg.lua
@@ -1,12 +1,9 @@
 melody.bg = Def.ActorFrame {}
 local bg = melody.bg
 --
-
 bg[#bg+1] = LoadActor("itg") .. {
 	Name= "itg",
-	File= "itg",
 	OnCommand= cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y),
-},
-
+}
 --
 return bg
