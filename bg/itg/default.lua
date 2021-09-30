119fc590137319685e1acca897d927e9824eb192
Jaezmien Naejara <jaezmien.naejara@gmail.com>
2021-09-30 13:07:46 +0800

    Remove itg bg names

--- bg/itg/default.lua
+++ bg/itg/default.lua
@@ -1,36 +1,29 @@
 return Def.ActorFrame{
-  Name= "xtl_actor_q",
-  Def.Sprite{
-    Name= "xtl_actor_r",
-    OnCommand= cmd(zoomto,melody.SCREEN_WIDTH,melody.SCREEN_HEIGHT),
-    Texture= "gradient-back.png",
-  },
-  Def.Sprite{
-    Name= "xtl_actor_s",
-    OnCommand= cmd(horizalign,left;vertalign,bottom;x,-melody.SCREEN_CENTER_X;y,melody.SCREEN_CENTER_Y),
-    Texture= "squares-left.png",
-  },
-  Def.Sprite{
-    Name= "xtl_actor_t",
-    OnCommand= cmd(horizalign,right;vertalign,top;x,melody.SCREEN_CENTER_X;y,-melody.SCREEN_CENTER_Y),
-    Texture= "squares-right.png",
-  },
-  Def.Sprite{
-    Name= "xtl_actor_u",
-    OnCommand= cmd(zoomto,melody.SCREEN_WIDTH,melody.SCREEN_HEIGHT),
-    Texture= "gradient-overlay.png",
-  },
-  Def.Sprite{
-    Name= "xtl_actor_v",
-    OnCommand= cmd(zoomto,melody.SCREEN_WIDTH,melody.SCREEN_HEIGHT;diffusealpha,0.4;blend,"BlendMode_Add"),
-    Texture= "lines.png",
-  },
-  Def.Sprite{
-    Name= "xtl_actor_w",
-    Texture= "text.png",
-  },
-  Def.Quad{
-    Name= "xtl_actor_x",
-    OnCommand= cmd(zoomto,melody.SCREEN_WIDTH,melody.SCREEN_HEIGHT;diffuse,color('0,0,0,0.4')),
-  },
-}
+	Def.Sprite{
+	  OnCommand= cmd(zoomto,melody.SCREEN_WIDTH,melody.SCREEN_HEIGHT),
+	  Texture= "gradient-back.png",
+	},
+	Def.Sprite{
+	  OnCommand= cmd(horizalign,left;vertalign,bottom;x,-melody.SCREEN_CENTER_X;y,melody.SCREEN_CENTER_Y),
+	  Texture= "squares-left.png",
+	},
+	Def.Sprite{
+	  OnCommand= cmd(horizalign,right;vertalign,top;x,melody.SCREEN_CENTER_X;y,-melody.SCREEN_CENTER_Y),
+	  Texture= "squares-right.png",
+	},
+	Def.Sprite{
+	  OnCommand= cmd(zoomto,melody.SCREEN_WIDTH,melody.SCREEN_HEIGHT),
+	  Texture= "gradient-overlay.png",
+	},
+	Def.Sprite{
+	  OnCommand= cmd(zoomto,melody.SCREEN_WIDTH,melody.SCREEN_HEIGHT;diffusealpha,0.4;blend,"BlendMode_Add"),
+	  Texture= "lines.png",
+	},
+	Def.Sprite{
+	  Texture= "text.png",
+	},
+	Def.Quad{
+	  OnCommand= cmd(zoomto,melody.SCREEN_WIDTH,melody.SCREEN_HEIGHT;diffuse,color('0,0,0,0.4')),
+	},
+  }
+  
